f7fa13363c02339b775cc29d396b00ab5a501b7d
hfg-yyds <1841814080@qq.com>
2022-05-19 18:15:45 +0800

    swagger删除,bug解决

--- gulimall-pro-coupon/src/main/java/com/hacker/coupon/GulimallCouponApplication.java
+++ gulimall-pro-coupon/src/main/java/com/hacker/coupon/GulimallCouponApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * 1、如何使用Nacos作为配置中心统一管理配置
@@ -56,7 +55,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableCaching //开启本地缓存功能
-@EnableSwagger2
 @EnableAspectJAutoProxy(proxyTargetClass=true)
 public class GulimallCouponApplication {
     public static void main(String[] args) {
--- gulimall-pro-member/src/main/java/com/hacker/member/GulimallMemberApplication.java
+++ gulimall-pro-member/src/main/java/com/hacker/member/GulimallMemberApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * 1、想要远程调用别的服务
@@ -16,7 +15,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableFeignClients(basePackages = "com.hacker.member.feign")
 @EnableDiscoveryClient
 @SpringBootApplication
-@EnableSwagger2
 public class GulimallMemberApplication {
 
     public static void main(String[] args) {
