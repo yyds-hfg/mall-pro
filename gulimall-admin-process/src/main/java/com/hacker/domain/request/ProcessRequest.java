78b0e38b7f2a0cf1e5c406684e302c5a43ba5619
hfg-yyds <1841814080@qq.com>
2022-05-25 15:06:19 +0800

    流程参数对象与任务参数对象

---
+++ gulimall-admin-process/src/main/java/com/hacker/domain/request/ProcessRequest.java
@@ -0,0 +1,47 @@
+package com.hacker.domain.request;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.Map;
+
+/**
+ * 流程请求对象
+ *
+ * @Author: Zero
+ * @Date: 2022/5/25 11:44
+ * @Description:
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@ApiModel(value="ProcessRequest-流程提交参数对象")
+@Data
+public class ProcessRequest {
+
+	@ApiModelProperty(value="流程定义ID")
+	private String	processDefId;
+	
+	@ApiModelProperty(value="流程定义Key")
+	private String	processDefKey;
+	
+	@ApiModelProperty(value="流程发起者")
+	private String	starter;
+	
+	@ApiModelProperty(value="流程标题")
+	private	String	title;
+	
+	@ApiModelProperty(value="外部业务系统数据主键标识值")
+	private String processBusinessKey;
+	
+	@ApiModelProperty(value="流程变量键值对")
+	private Map<String, Object> variables;
+   
+	public static String REJECT_TO_START ="1";
+	public static String REJECT_TO_LAST ="2";
+	public static String REJECT_TO_TARGET ="3";
+   
+	public static String WITHDRAW_TO_START ="1";
+	public static String WITHDRAW_TO_TARGET ="2";
+	
+}
---
+++ gulimall-admin-process/src/main/java/com/hacker/domain/request/TaskRequest.java
@@ -0,0 +1,64 @@
+package com.hacker.domain.request;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.Map;
+
+/**
+ * 任务参数对象
+ *
+ * @Author: Zero
+ * @Date: 2022/5/25 11:46
+ * @Description:
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@ApiModel(value="TaskRequest-任务参数对象")
+@Data
+public class TaskRequest {
+	@ApiModelProperty(value="流程实例ID")
+	private String	processInstId;
+	
+	@ApiModelProperty(value="流程定义ID")
+	private String processDefId;
+	
+	@ApiModelProperty(value="任务ID")
+	private String	taskId;
+	
+	@ApiModelProperty(value="任务Key")
+	private String	taskDefKey;
+ 
+	@ApiModelProperty(value="任务处理人")
+	private String	userId;
+	
+	@ApiModelProperty(value="流程变量键值对")
+	private Map<String, Object> variables;
+	
+	@ApiModelProperty(value="任务变量键值对")
+	private Map<String, Object> localVariables;
+	
+	@ApiModelProperty(value="下一节点任务处理人")
+	private String	nextUserId;
+	
+	@ApiModelProperty(value="驳回类型，1：起草节点，2：上一节点，3：目标节点")
+	private String	rejectType;
+	
+	@ApiModelProperty(value="目标节点Id")
+	private String	toActId;
+	
+	@ApiModelProperty(value="跳转类型，1：往前跳转，2：往回跳转")
+	private String	jumpType;
+
+	public static String REJECT_TO_START ="1";
+	public static String REJECT_TO_LAST ="2";
+	public static String REJECT_TO_TARGET ="3";
+	
+	public static String WITHDRAW_TO_START ="1";
+	public static String WITHDRAW_TO_TARGET ="2";
+	
+	public static String JUMP_FORWARD ="1";
+	public static  String JUMP_BACK ="2";
+	
+}
