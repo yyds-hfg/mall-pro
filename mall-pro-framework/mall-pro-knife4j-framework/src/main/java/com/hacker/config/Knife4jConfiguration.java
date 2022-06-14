1d37ac7bbda73e93e818be2ef07dfb52a4d896d1
hfg-yyds <1841814080@qq.com>
2022-06-14 15:19:38 +0800

    mall-pro-knife4j-framework项目添加

---
+++ mall-pro-framework/mall-pro-knife4j-framework/src/main/java/com/hacker/config/Knife4jConfiguration.java
@@ -0,0 +1,52 @@
+package com.hacker.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+/**
+ * @Author: Zero
+ * @Date: 2022/5/19 15:37
+ * @Description:
+ */
+@Configuration
+@EnableSwagger2WebMvc
+public class Knife4jConfiguration {
+
+    @Value("${knife4j.description:商城服务}")
+    private String description;
+
+    @Value("${knife4j.groupName:默认分组-商城服务}")
+    private String groupName;
+
+    @Value("${knife4j.basePackage:com.hacker}")
+    private String basePackage;
+
+    @Value("${knife4j.version:1.0}")
+    private String version;
+
+    @Bean(value = "defaultApi2")
+    public Docket defaultApi2() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(new ApiInfoBuilder()
+                        //.title("swagger-bootstrap-ui-demo RESTful APIs")
+                        .description(description)
+                        .termsOfServiceUrl("https://www.google.com/")
+                        .version(version)
+                        .build())
+                //分组名称
+                .groupName(groupName)
+                .select()
+                //这里指定Controller扫描包路径
+                .apis(RequestHandlerSelectors.basePackage(basePackage))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+}
