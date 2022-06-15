bf8d43c18b8ba13822ee8e11978389079e599e64
hfg-yyds <1841814080@qq.com>
2022-06-15 09:03:20 +0800

    mall-pro-ware

---
+++ mall-pro-coupon/src/main/java/com/hacker/coupon/service/impl/HomeSubjectSpuServiceImpl.java
@@ -0,0 +1,30 @@
+package com.hacker.coupon.service.impl;
+
+import com.hacker.coupon.dao.HomeSubjectSpuDao;
+import com.hacker.coupon.entity.HomeSubjectSpuEntity;
+import com.hacker.coupon.service.HomeSubjectSpuService;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hacker.common.utils.PageUtils;
+import com.hacker.common.utils.Query;
+
+
+@Service("homeSubjectSpuService")
+public class HomeSubjectSpuServiceImpl extends ServiceImpl<HomeSubjectSpuDao, HomeSubjectSpuEntity> implements HomeSubjectSpuService {
+
+    @Override
+    public PageUtils queryPage(Map<String, Object> params) {
+        IPage<HomeSubjectSpuEntity> page = this.page(
+                new Query<HomeSubjectSpuEntity>().getPage(params),
+                new QueryWrapper<HomeSubjectSpuEntity>()
+        );
+
+        return new PageUtils(page);
+    }
+
+}
